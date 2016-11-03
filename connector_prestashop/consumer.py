fa7bcf857ba43f43e67d89cbed1302b183fecf60
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2016-11-03 08:52:59 +0100

    [IMP][8.0] connector_prestashop: Make to_odoo alias in this module instead of in connector module.

--- connector_prestashop/consumer.py
+++ connector_prestashop/consumer.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.addons.connector.connector import Binder
 from openerp.addons.connector.event import on_record_write
 from openerp.addons.connector_ecommerce.event import on_tracking_number_added
 from .unit.deleter import export_delete_record
 from .connector import get_environment
+from .unit.binder import PrestashopBinder
 from .unit.exporter import export_record
 from .models.product_template.exporter import export_inventory
 from .models.sale_order_state.exporter import export_sale_state
@@ -66,7 +66,7 @@ def delay_unlink(session, model_name, record_id, priority=20):
     model = session.env[model_name]
     record = model.browse(record_id)
     env = get_environment(session, model_name, record.backend_id.id)
-    binder = env.get_connector_unit(Binder)
+    binder = env.get_connector_unit(PrestashopBinder)
     external_id = binder.to_backend(record_id)
     if external_id:
         export_delete_record.delay(session, model_name,
@@ -84,7 +84,7 @@ def delay_unlink_all_bindings(session, model_name, record_id, priority=20):
         prestashop_model_name = bind_record._name
         env = get_environment(
             session, prestashop_model_name, bind_record.backend_id.id)
-        binder = env.get_connector_unit(Binder)
+        binder = env.get_connector_unit(PrestashopBinder)
         ext_id = binder.to_backend(bind_record.id)
         if ext_id:
             export_delete_record.delay(
